f696dcf0458a228bf2d4048a903ec0ee1a0b26d9
TemenuzhkaG <temenujka_211@abv.bg>
2022-08-03 12:57:52 +0300

    update condition 1 and add condition 2 in example 2 from week 2

--- week-2/ex-2/scripts/script.js
+++ week-2/ex-2/scripts/script.js
@@ -1,73 +1,124 @@
-const yearNow = 2022;
 
-function getRandomNumber() {
+const yearNow = 2022;
+const boundaryYear = 1965;
+const digitsCollection = [];
+
+function getFirstRandomNumber() {
     return Math.floor(Math.random() * 7) + 1;
+}
+
+function getSecondRandomNumber() {
+  return Math.floor(Math.random() * 9) + 1;
 }
 
 var clientBirthYear = prompt("Моля въведете Вашата година на раждане !");
 var clientBirthYearResult = parseInt(clientBirthYear);
-
-console.log(clientBirthYearResult);
-
-var isFirstNumberEven             =  clientBirthYearResult > 1965;
-var isFirstNumberOdd              =  clientBirthYearResult < 1965;
-var isClientBirthYearSpecialCase  =  clientBirthYearResult === 1965;
-var firstNumber;
-
 var clientYears = yearNow - clientBirthYearResult;
+
+var isFirstNumberEven             =  clientBirthYearResult > boundaryYear;
+var isFirstNumberOdd              =  clientBirthYearResult < boundaryYear;
+var isClientBirthYearSpecialCase  =  clientBirthYearResult === boundaryYear;
+var firstNumber;
 
 if(isClientBirthYearSpecialCase) {
     firstNumber = 0;
-}
-console.log(getRandomNumber());
+}else{
 
-var randomNumberResult = clientYears / getRandomNumber();
+  var randomNumberResult = Math.round(clientYears / getFirstRandomNumber());
 
-if(isFirstNumberEven) { 
+  if(isFirstNumberEven) { 
+  
+     var isRandomNumberEven = randomNumberResult % 2 === 0;
 
-   var isRandomNumberEven = randomNumberResult % 2 === 0;
+     if(isRandomNumberEven) {
 
-   if(isRandomNumberEven) {
-     firstNumber = randomNumberResult;
-   }else {
+       var isEvenNumberMoreThanTwoDigits = randomNumberResult >= 10;
+          if(isEvenNumberMoreThanTwoDigits) {
 
-    var oddRandomNumberResult        = randomNumberResult + 1;
-    var oddRandomNumberToString      = oddRandomNumberResult.toString();
-    var isNumberHaveMoreThanTwoDigits = oddRandomNumberResult >= 10;
+            var evenNumberToString = randomNumberResult.toString();
+            firstNumber = evenNumberToString[0];
 
-    if(isNumberHaveMoreThanTwoDigits) {
+          } else {
+            firstNumber = randomNumberResult;
+          }
+     }else {
+  
+      var oddRandomNumberResult          = randomNumberResult + 1;
+      var evenRandomNumberToString       = oddRandomNumberResult.toString();
+      var isNumberHaveMoreThanTwoDigits  = oddRandomNumberResult >= 10;
 
-        for (let i = 0; i < oddRandomNumberToString.length; i++) {
-            firstNumber = oddRandomNumberToString.charAt(0);
-            break;
+      if(isNumberHaveMoreThanTwoDigits) {
+        firstNumber = evenRandomNumberToString[0];
+        }else {
+          firstNumber = randomNumberResult + 1;
         }
+     }
+  } else {
 
-      }else {
-        firstNumber = randomNumberResult + 1;
-      }
-   }
-} else {
-    var isRandomNumberOdd = randomNumberResult % 2 !== 0;
+      var isRandomNumberOdd = randomNumberResult % 2 !== 0;
 
-    if(isRandomNumberOdd) {
-        firstNumber = randomNumberResult;
-      }else {
-   
-       var oddRandomNumberResult         = randomNumberResult + 1;
-       var oddRandomNumberToString       = oddRandomNumberResult.toString();
-       var isNumberHaveMoreThanTwoDigits = oddRandomNumberResult >= 10;
-   
-       if(isNumberHaveMoreThanTwoDigits) {
-   
-           for (let i = 0; i < oddRandomNumberToString.length; i++) {
-               firstNumber = oddRandomNumberToString.charAt(0);
-               break;
+      if(isRandomNumberOdd) {
+
+          var isOddNumberMoreThanTwoDigits = randomNumberResult >= 10;
+          if(isOddNumberMoreThanTwoDigits) {
+
+            var oddNumberToString = randomNumberResult.toString();
+            firstNumber = oddNumberToString[0];
+          
+          } else {
+            firstNumber = randomNumberResult;
+          }
+        }else {
+     
+         var oddRandomNumberResult          = randomNumberResult + 1;
+         var oddRandomNumberToString        = oddRandomNumberResult.toString();
+         var isNumberHaveMoreThanTwoDigits  = oddRandomNumberResult >= 10;
+
+         if(isNumberHaveMoreThanTwoDigits) {
+             firstNumber = oddRandomNumberToString[0];
+
+           }else {
+             firstNumber = randomNumberResult + 1;
            }
-   
-         }else {
-           firstNumber = randomNumberResult + 1;
-         }
-    }
+      }
+  }
 }
 
-console.log(firstNumber);
+var isFirstNumberEqualNine = firstNumber === 9;
+
+if(isFirstNumberEqualNine) {
+   secondNumber = 0;
+}else {
+  var clientGender = prompt("Моля въведете Вашия пол !");
+  var isGenderMan   = clientGender.toLowerCase() === "мъжки";
+  var isGenderWoman = clientGender.toLowerCase() === "женски";
+  var isUserSkipThisQuestion = clientGender !== isGenderMan && clientGender !== isGenderWoman;
+  var secondNumber;
+
+  if(isUserSkipThisQuestion) {
+   
+    var isRandomNumberEqualToGenderMan   = getSecondRandomNumber() === 1;
+    var isRandomNumberEqualToGenderWoman = getSecondRandomNumber() === 3;
+  
+    while(isRandomNumberEqualToGenderMan   === false || 
+          isRandomNumberEqualToGenderWoman === false) {
+      var generateNewRandomNumber = getSecondRandomNumber();
+      var isNewRandomNumberDifferent = generateNewRandomNumber !== 1 && 
+                                       generateNewRandomNumber !== 3;
+
+      if(isNewRandomNumberDifferent) {
+         secondNumber = generateNewRandomNumber;
+         break;
+      }
+        continue;
+    }
+  }else {
+
+    if(isGenderMan) {
+       secondNumber = 1;
+    } 
+    if(isGenderWoman) {
+      secondNumber = 3;
+    }
+  }
+}
